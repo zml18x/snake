a200c9d0b1d6d37cba5ebe3be876bd82deca1d06
ZML <zml18v@gmail.com>
2023-11-27 18:11:09 +0100

    Naprawiono błąd przez który długość węża nie zwiększała się po zjedzeniu owocu.

---
+++ Snake/SnakeSegment.cs
@@ -0,0 +1,16 @@
+﻿namespace Snake
+{
+    public class SnakeSegment
+    {
+        public int Xpos {  get; set; }
+        public int Ypos { get; set; }
+
+
+
+        public SnakeSegment(int xpos, int ypos)
+        {
+            Xpos = xpos;
+            Ypos = ypos;
+        }
+    }
+}
